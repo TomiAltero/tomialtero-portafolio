faf412d93839c335d3cb697084e9eda277ecd5e4
TomiAltero <tomasaltero@gmail.com>
2024-05-09 17:36:26 -0300

    integracion tailwind

---
+++ react-app-portfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
