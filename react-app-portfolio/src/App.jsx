1f60163ffe99be7a2906f9ed9c8eec8b9fe1cb3a
TomiAltero <tomasaltero@gmail.com>
2024-05-09 18:38:07 -0300

    barra de navegacion

--- react-app-portfolio/src/App.jsx
+++ react-app-portfolio/src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
+import Navbar from "./components/navbar.jsx";
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-10 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-gray-800 mb-5">¡Hola, Mundo!</h1>
-        <p className="text-gray-600">¡Bienvenido a mi aplicación con React y Tailwind CSS!</p>
-      </div>
+    <div>
+      <Navbar />
     </div>
   );
+
 }
 
 export default App;
--- react-app-portfolio/tailwind.config.js
+++ react-app-portfolio/tailwind.config.js
@@ -8,4 +8,5 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer'),
   ],
+  darkMode: 'media',
 }
